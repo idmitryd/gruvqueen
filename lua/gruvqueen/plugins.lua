c9e6cfcea6e1a0db58ce8a8e910e6382e08f7e4a
idmitryd <dolzhikov.d.a@gmail.com>
2021-12-15 14:34:17 +0300

    Add more plugins support

--- lua/gruvqueen/plugins.lua
+++ lua/gruvqueen/plugins.lua
@@ -63,6 +63,10 @@ local function init(c, config)
         NvimTreeGitNew = {fg = c.aqua},
         NvimTreeGitDeleted = {fg = c.red},
         NvimTreeWindowPicker = {fg = c.fg0, bg = c.bg3, style = config.disable_bold and "NONE" or "bold"},
+        NvimTreeNormal = { fg = c.fg0, bg = c.bg_statusline1 },
+        NvimTreeVertSplit = { fg = c.bg_statusline1, bg = c.bg_statusline1 },
+        NvimTreeStatusLine = { fg = c.bg_statusline1, bg = c.bg_statusline1 },
+        NvimTreeStatusLineNC = { fg = c.bg_statusline1, bg = c.bg_statusline1 },
 
         -- undotree
         UndotreeSavedBig = {fg = c.purple, style = 'bold'},
@@ -86,6 +90,87 @@ local function init(c, config)
         StartifyHeader = {fg = c.orange},
         StartifySpecial = {fg = c.grey1},
         StartifyFooter = {fg = c.grey1},
+
+        -- BarBar
+        BufferCurrent = { fg = c.fg0, bg = c.bg0, },
+        BufferCurrentIndex = { fg = c.fg0, bg = c.bg0, },
+        BufferCurrentMod = { fg = c.yellow, bg = c.bg0, },
+        BufferCurrentSign = { fg = c.green, bg = c.bg0},
+        BufferCurrentTarget = { fg = c.orange, bg = c.bg0, style = "bold", },
+        BufferVisible = { fg = c.fg0, bg = c.bg0, },
+        BufferVisibleIndex = { fg = c.fg0, bg = c.bg0, },
+        BufferVisibleMod = { fg = c.yellow, bg = c.bg0, },
+        BufferVisibleSign = { fg = c.grey2, bg = c.bg0, },
+        BufferVisibleTarget = { fg = c.orange, bg = c.bg0, style = "bold", },
+        BufferInactive = { fg = c.grey1, bg = c.bg3, },
+        BufferInactiveIndex = { fg = c.grey1, bg = c.bg3, },
+        BufferInactiveMod = { fg = c.grey1, bg = c.bg3, },
+        BufferInactiveSign = { fg = c.grey1, bg = c.bg3},
+        BufferInactiveTarget = { fg = c.green, bg = c.bg3, style = "bold"},
+        BufferOffset = { fg = c.fg0, bg = c.bg_statusline1 },
+        BufferTabpages = { fg = c.purple },
+
+        -- dapui
+        DapUIVariable = { fg = c.fg0, bg = c.bg0 },
+        DapUIScope = { fg = c.green, },
+        DapUIType = { fg = c.yellow, },
+        DapUIValue = { fg = c.purple, bg = c.bg0 },
+        DapUIModifiedValue = { fg = c.aqua, style = "bold", },
+        DapUIDecoration = { fg = c.green, },
+        DapUIThread = { fg = c.purple, },
+        DapUIStoppedThread = { fg = c.green, },
+        DapUIFrameName = { fg = c.fg0, bg = c.bg0 },
+        DapUISource = { fg = c.yellow, },
+        DapUILineNumber = { fg = c.aqua, },
+        DapUIFloatBorder = { fg = c.bg_statusline3, },
+        DapUIWatchesEmpty = { fg = c.red, },
+        DapUIWatchesValue = { fg = c.purple, },
+        DapUIWatchesError = { fg = c.red, },
+        DapUIBreakpointsPath = { fg = c.green, },
+        DapUIBreakpointsInfo = { fg = c.purple, },
+        DapUIBreakpointsCurrentLine = { fg = c.aqua, style = "bold", },
+        DapUIBreakpointsLine = { fg = c.aqua, },
+
+        -- lspsaga
+        LspSagaBorderTitle = { fg = c.orange },
+        LspSagaHoverBorder = { fg = c.grey1, bg = c.bg0, },
+        LspSagaRenameBorder = { fg = c.aqua },
+        LspSagaDefPreviewBorder = { fg = c.grey1, bg = c.bg0, },
+        LspSagaCodeActionBorder = { fg = c.green, },
+        LspSagaFinderSelection = { fg = c.fg0 },
+        LspSagaCodeActionTitle = { fg = c.orange },
+        LspSagaCodeActionContent = { fg = c.fg0 },
+        LspSagaSignatureHelpBorder = { fg = c.grey1, bg = c.bg0, },
+        LspSagaDiagnosticBorder = { fg = c.fg0 },
+        LspSagaDiagnosticTruncateLine = { fg = c.fg0 },
+        LspSagaLspFinderBorder = { fg = c.blue },
+        LspSagaShTruncateLine = { fg = c.fg0, },
+        LspSagaDocTruncateLine = { fg = c.fg0, },
+        LspSagaCodeActionTruncateLine = { fg = c.fg0, },
+
+        -- Hop
+        HopNextKey = { fg = c.orange, style = "bold"  },
+        HopNextKey1 = { fg = c.green, style = "bold" },
+        HopNextKey2 = { fg = c.aqua, style = "bold" },
+
+        -- Notify
+        NotifyERRORBorder = { fg = c.red, },
+        NotifyWARNBorder = { fg = c.orange, },
+        NotifyINFOBorder = { fg = c.blue, },
+        NotifyDEBUGBorder = { fg = c.purple, },
+        NotifyTRACEBorder = { fg = c.aqua, },
+
+        NotifyERRORIcon = { fg = c.red, },
+        NotifyWARNIcon = { fg = c.orange, },
+        NotifyINFOIcon = { fg = c.blue, },
+        NotifyDEBUGIcon = { fg = c.purple, },
+        NotifyTRACEIcon = { fg = c.aqua, },
+
+        NotifyERRORTitle = { fg = c.red, },
+        NotifyWARNTitle = { fg = c.orange, },
+        NotifyINFOTitle = { fg = c.blue, },
+        NotifyDEBUGTitle = { fg = c.purple, },
+        NotifyTRACETitle = { fg = c.aqua, },
     }
 
     return plugins
