cc16be564f0d19ab269b15f61580073fc0e7a2bf
idmitryd <dolzhikov.d.a@gmail.com>
2021-12-23 06:40:19 +0300

    Add possibility to link highlight group

--- lua/gruvqueen/util.lua
+++ lua/gruvqueen/util.lua
@@ -1,16 +1,22 @@
 local M = {}
 
 local function highlight(group, properties)
-    local bg = properties.bg == nil and "" or "guibg=" .. properties.bg
-    local fg = properties.fg == nil and "" or "guifg=" .. properties.fg
-    local sp = properties.sp == nil and "" or "guisp=" .. properties.sp
-    local style = properties.style == nil and "" or "gui=" .. properties.style
+    if properties.link then
+        local cmd = table.concat({
+            "highlight! link", group, properties.link
+        }, " ")
+        vim.api.nvim_command(cmd)
+    else
+        local bg = properties.bg == nil and "" or "guibg=" .. properties.bg
+        local fg = properties.fg == nil and "" or "guifg=" .. properties.fg
+        local sp = properties.sp == nil and "" or "guisp=" .. properties.sp
+        local style = properties.style == nil and "" or "gui=" .. properties.style
 
-    local cmd = table.concat({
-        "highlight", group, bg, fg, sp, style
-    }, " ")
-
-    vim.api.nvim_command(cmd)
+        local cmd = table.concat({
+            "highlight", group, bg, fg, sp, style
+        }, " ")
+        vim.api.nvim_command(cmd)
+    end
 end
 
 function M.initialise(skeleton)
